1547a7c45a63e3d9708571fe0913b37c42317161
Vincent Garrigues <vincent.garrigues@gmail.com>
2016-03-05 14:19:22 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'soundcloudql-swiftTests' do
+  pod 'Quick', '~> 0.9.1'
+  pod 'Nimble', '~> 3.1.0'
+end
---
+++ soundcloudql-swiftTests/soundcloudql_swiftTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import soundcloudql_swift
+
+class soundcloudql_swiftTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+        self.measureBlock {
+        }
+    }
+
+}
