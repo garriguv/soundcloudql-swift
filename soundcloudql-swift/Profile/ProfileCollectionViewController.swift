ccc94d6449630908ee49247879ab6fa7a42fa3d4
Vincent Garrigues <vincent.garrigues@gmail.com>
2016-03-06 16:21:07 +0100

    Profile GraphQL query

--- soundcloudql-swift/Profile/ProfileCollectionViewController.swift
+++ soundcloudql-swift/Profile/ProfileCollectionViewController.swift
@@ -2,4 +2,16 @@ import Foundation
 import UIKit
 
 class ProfileCollectionViewController: UICollectionViewController {
+
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+
+    let controller = ApiController.sharedInstance
+    controller.fetch(withGraphQLQuery: "profile", variables: [ "id": "2" ]) {
+      (json: [String: AnyObject]?, error: ApiControllerError?) in
+      print("json \(json)")
+      print("error \(error)")
+    }
+  }
+
 }
